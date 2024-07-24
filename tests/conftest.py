a06f6520db115e904adb8ab5bdaee6ed54674b37
Alexander Andrew Antoun <98501829+Alexantoun@users.noreply.github.com>
2024-07-24 17:43:05 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import sys
 import os
+load_dotenv()
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
