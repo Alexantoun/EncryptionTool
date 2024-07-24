671012f5913736593ac7d2dec89b25de21b5200c
Alex <alex@rig.myguest.virtualbox.org>
2024-07-24 17:59:49 -0500

    remove unecessary files

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import sys
-import os
-load_dotenv()
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
